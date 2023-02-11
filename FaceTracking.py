a183ecb824a93f93b9a272dbbd2b4eaf01bb6330
sv2441 <sinu2441@gmail.com>
2023-02-12 02:40:34 +0530

    all basic codes

--- FaceTracking.py
+++ FaceTracking.py
@@ -2,65 +2,28 @@ import cv2
 import mediapipe as mp
 import time
 
+cap = cv2.VideoCapture(0)
+ptime = 0
 
-class FaceMeshDetector():
+mpdraw = mp.solutions.drawing_utils
+mpFashMwsh = mp.solutions.face_mesh
+faceMesh = mpFashMwsh.FaceMesh(max_num_faces=2)
+drawSpec = mpdraw.DrawingSpec(thickness=1, circle_radius=2)
 
-    def __init__(self, staticMode=False, maxFaces=2, minDetectionCon=1, minTrackCon=0.5):
-
-        self.staticMode = staticMode
-        self.maxFaces = maxFaces
-        self.minDetectionCon = minDetectionCon
-        self.minTrackCon = minTrackCon
-        # self.modelComplex = model_complexity
-
-        self.mpDraw = mp.solutions.drawing_utils
-        self.mpFaceMesh = mp.solutions.face_mesh
-        self.faceMesh = self.faceMesh = self.mpFaceMesh.FaceMesh(static_image_mode=self.staticMode,
-                                                                 max_num_faces=self.maxFaces,
-                                                                 min_detection_confidence=self.minDetectionCon,
-                                                                 min_tracking_confidence=self.minTrackCon)
-        self.drawSpec = self.mpDraw.DrawingSpec(thickness=1, circle_radius=2)
-
-    def findFaceMesh(self, img, draw=True):
-        self.imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self.results = self.faceMesh.process(self.imgRGB)
-        faces = []
-        if self.results.multi_face_landmarks:
-            for faceLms in self.results.multi_face_landmarks:
-                if draw:
-                    self.mpDraw.draw_landmarks(img, faceLms, self.mpFaceMesh.FACE_CONNECTIONS,
-                                               self.drawSpec, self.drawSpec)
-                face = []
-                for id, lm in enumerate(faceLms.landmark):
-                    # print(lm)
-                    ih, iw, ic = img.shape
-                    x, y = int(lm.x*iw), int(lm.y*ih)
-                    # cv2.putText(img, str(id), (x, y), cv2.FONT_HERSHEY_PLAIN,
-                    #           0.7, (0, 255, 0), 1)
-
-                    # print(id,x,y)
-                    face.append([x, y])
-                faces.append(face)
-        return img, faces
-
-
-def main():
-    cap = cv2.VideoCapture("test.mp4")
-    pTime = 0
-    detector = FaceMeshDetector(maxFaces=2)
-    while True:
-        success, img = cap.read()
-        img, faces = detector.findFaceMesh(img)
-        if len(faces) != 0:
-            print(faces[0])
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-        cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN,
-                    3, (0, 255, 0), 3)
-        cv2.imshow("Image", img)
-        cv2.waitKey(1)
-
-
-if __name__ == "__main__":
-    main()
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceMesh.process(imgRGB)
+    if results.multi_face_landmarks:
+        for facelms in results.multi_face_landmarks:
+            mpdraw.draw_landmarks(
+                img, facelms, mpFashMwsh.FACEMESH_CONTOURS, drawSpec, drawSpec)
+            for id, im in enumerate(facelms.landmark):
+                ih, iw, ic = img.shape
+    ctime = time.time()
+    fps = 1/(ctime-ptime)
+    ptime = ctime
+    cv2.putText(img, str(int(fps)), (70, 50),
+                cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+    cv2.imshow("image", img)
+    cv2.waitKey(1)
