65fbf693868ccfdf250de639ae09608b6f560f09
sv2441 <sinu2441@gmail.com>
2023-02-12 02:30:28 +0530

    adding hand tracking

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ while True:
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN,
-                3, (255, 0, 0), 3)
+    # cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN,
+    #             3, (255, 0, 0), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
